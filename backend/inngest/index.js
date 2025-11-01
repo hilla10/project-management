d47481ccb5f5fe4bcba2add3e862652d97f77ada
hilla10 <hillaman592@gmail.com>
2025-11-01 13:10:13 +0300

    remove unnecessary data property from Prisma delete operation in syncUserDeletion

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -31,11 +31,6 @@ const syncUserDeletion = inngest.createFunction(
       where: {
         id: data.id,
       },
-      data: {
-        email: data?.email_addresses[0]?.email_address,
-        name: data?.first_name + ' ' + data?.last_name,
-        image: data?.image_url,
-      },
     });
   }
 );
@@ -50,6 +45,11 @@ const syncUserUpdation = inngest.createFunction(
       data: {
         where: {
           id: data.id,
+        },
+        data: {
+          email: data?.email_addresses[0]?.email_address,
+          name: data?.first_name + ' ' + data?.last_name,
+          image: data?.image_url,
         },
       },
     });
